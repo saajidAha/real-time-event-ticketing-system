19ec7896cf8f7ef0c742a8a2ba03b8ee2ece4bcf
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-04 21:16:20 +0530

    implemented logic to start/ stop system

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/cli/CommandLineInterface.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/cli/CommandLineInterface.java
@@ -14,38 +14,40 @@ public class CommandLineInterface implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        /*
-         * Prompt user and initialize configuration;
-         */
-        int totalTickets = validateInput("Enter the total number of tickets");
-        int ticketReleaseRate = validateInput("Enter the ticket release rate");
-        int customerRetreivalRate = validateInput("Enter the customer retrieval rate");
-        int maxTicketCapacity = validateInput("Enter the maximum ticket capcity");
+        // Prompt user
+        int totalTickets = validateConfig("Enter the total number of tickets");
+        int ticketReleaseRate = validateConfig("Enter the ticket release rate");
+        int customerRetreivalRate = validateConfig("Enter the customer retrieval rate");
+        int maxTicketCapacity = validateConfig("Enter the maximum ticket capcity");
 
+        // Serialize
         Configuration config = new Configuration(totalTickets, ticketReleaseRate, customerRetreivalRate, maxTicketCapacity);
         config.serialize();
-        Configuration value = config.deSerialize();
-        System.out.println(value);
 
-
+        // Start / Stop system
+        if ( validateSystemStart() ){
+            System.out.println("System started successfully");
+        } else{
+            System.out.println("System process aborted.");
+        }
 
     }
 
     /**
-     * Method that validates user inputs
+     * Validates user inputs
      * @param promptMessage The prompt message to be displayed to the user
      * @return A positive integer
      */
-    private static int validateInput(String promptMessage){
-        int value = 0;
+    private static int validateConfig(String promptMessage){
+        int response = 0;
         boolean isValidated = false;
 
         while (!isValidated){
             try {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println(promptMessage + ":   ");
-                value = scanner.nextInt();
-                if (value > 0){
+                response = scanner.nextInt();
+                if (response > 0){
                     isValidated = true;
                 }else{
                     System.out.println("Input should be a positive integer.");
@@ -54,6 +56,34 @@ public class CommandLineInterface implements CommandLineRunner {
                 System.out.println("Invalid input please try again.");
             }
         }
-        return value;
+        return response;
+    }
+
+    /**
+     * Validates user input to start / stop system
+     * @return True if response is "yes", false if "no"
+     */
+    private static boolean validateSystemStart(){
+        boolean startSystem = false;
+        boolean validated = false;
+
+        while (!validated){
+            try{
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Do you want to start the system? Enter (Yes / No): ");
+                String response = scanner.nextLine();
+                if (response.equalsIgnoreCase("yes")){
+                    validated = true;
+                    startSystem = true;
+                } else if (response.equalsIgnoreCase("no")) {
+                    validated = true;
+                } else {
+                    System.out.println("Invalid response. Please type 'yes' or 'no'");
+                }
+            }catch (Exception e){
+                System.out.println("Invalid response. Please type 'yes' or 'no'");
+            }
+        }
+        return startSystem;
     }
 }
