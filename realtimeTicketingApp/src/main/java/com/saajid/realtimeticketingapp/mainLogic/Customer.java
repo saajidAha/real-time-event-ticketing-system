0cfd8df3466ea17aded1ece17b4ad06e7a177ed7
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-06 02:49:04 +0530

    added customer retreival rate functionality

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Customer.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Customer.java
@@ -4,10 +4,12 @@ package com.saajid.realtimeticketingapp.mainLogic;
  * This class represents the Customer threads
  */
 public class Customer implements Runnable{
-    public TicketPool ticketPool;
+    private TicketPool ticketPool;
+    private int customerRetreivalRate;
 
-    public Customer(TicketPool ticketPool){
+    public Customer(TicketPool ticketPool, int customerRetreivalRate){
         this.ticketPool = ticketPool;
+        this.customerRetreivalRate = customerRetreivalRate;
     }
 
     @Override
@@ -15,7 +17,7 @@ public class Customer implements Runnable{
         for (int i=0; i<10; i++){
             ticketPool.removeTicket();
             try {
-                Thread.sleep(100);
+                Thread.sleep( customerRetreivalRate );
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
