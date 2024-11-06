ffbd841d35beaccd0274fdfcaf2fdea65b7672e0
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-07 00:13:44 +0530

    added meanigful comments

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Simulator.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Simulator.java
@@ -1,5 +1,8 @@
 package com.saajid.realtimeticketingapp.mainLogic;
 
+/**
+ * This class is responsible for initializing and simulating multiple vendor and customer threads
+ */
 public class Simulator {
     private Configuration config;
     private TicketPool ticketPool;
@@ -10,15 +13,21 @@ public class Simulator {
         config.serialize();
     }
 
-    public void simulate(){
-        for (int i=1; i<=5; i++){
+    /**
+     * Simulates multithreaded environment where multiple customers and vendors interact with the ticket pool
+     * @param numOfVendors The number of vendors that are going to be releasing tickets
+     * @param numOfCustomers The number of customers that are going to purchase tickets
+     */
+    public void simulate(int numOfVendors, int numOfCustomers){
+
+        for (int i=1; i<=numOfVendors; i++){
             String name = "Vendor_#" + i;
             Runnable vendor = new Vendor(this.ticketPool, config.getTicketReleaseRate());
             Thread vendorThread = new Thread(vendor, name);
             vendorThread.start();
         }
 
-        for (int i=1; i<=5; i++){
+        for (int i=1; i<=numOfCustomers; i++){
             String name = "Customer_#" + i;
             Runnable customer = new Customer(this.ticketPool, config.getCustomerRetreivalRate());
             Thread customerThread = new Thread(customer, name );
