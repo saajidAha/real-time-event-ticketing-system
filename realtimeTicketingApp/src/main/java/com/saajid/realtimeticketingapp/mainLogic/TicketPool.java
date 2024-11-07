780981b40c6a765582eba37e3cb43538c3e572c6
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-07 15:36:26 +0530

    added meaningful comments

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/TicketPool.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/TicketPool.java
@@ -9,7 +9,10 @@ public class TicketPool {
     private Vector<Ticket> tickets;
     private Configuration config;
 
-
+    /**
+     * Initializes the vector with specified initial number of tickets
+     * @param config Configuration object used to extract configuration parameters by the user
+     */
     public TicketPool(Configuration config){
         this.tickets = new Vector<>();
         this.config = config;
@@ -19,6 +22,10 @@ public class TicketPool {
         System.out.println("System initialized with " + this.config.getTotalTickets() + " tickets: Ticket ID's: " + this.tickets.toString() );
     }
 
+    /**
+     * Adds tickets as long as the issued tickets does not exceed the maximum ticket capacity specified
+     * @param ticket The ticket that will be added to the vector
+     */
     public synchronized void addTicket(Ticket ticket){
         while ( this.tickets.size()
                 == this.config.getMaxTicketCapacity() ){
@@ -28,13 +35,14 @@ public class TicketPool {
                 throw new RuntimeException(e);
             }
         }
-        if ( this.tickets.size() < this.config.getMaxTicketCapacity() ){
             this.tickets.add(ticket);
             System.out.println(Thread.currentThread().getName() + " issued ticket successfully: [Ticket ID:  " + ticket.getTicketID() + "]");
             notifyAll();
-        }
     }
 
+    /**
+     * Removes tickets in FIFO order from the vector as long as it is not empty
+     */
     public synchronized void removeTicket(){
         while (this.tickets.isEmpty()){
             try {
