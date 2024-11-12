310c5dc440ef29b7e20a3fcb159b1b4016a1dbe4
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-12 18:47:19 +0530

    imporoved logic

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Simulator.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Simulator.java
@@ -1,5 +1,7 @@
 package com.saajid.realtimeticketingapp.mainLogic;
 
+import com.saajid.realtimeticketingapp.server.TicketController;
+
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
@@ -8,12 +10,11 @@ import static com.saajid.realtimeticketingapp.mainLogic.LoggerHandler.logInfo;
 
 /**
  * This class is responsible for initializing and simulating multiple vendor and customer threads
+ *
  */
 public class Simulator {
     private Configuration config;
     private TicketPool ticketPool;
-    private ArrayList<Thread> vendorThreads;
-    private ArrayList<Thread> customerThreads;
     private ArrayList<Thread> allThreads;
     private static Logger logger = Logger.getLogger(Simulator.class.getName());
 
@@ -24,47 +25,49 @@ public class Simulator {
     public Simulator(Configuration config){
         this.config = config;
         this.ticketPool = new TicketPool(config);
-        this.vendorThreads = new ArrayList<>();
-        this.customerThreads = new ArrayList<>();
-        this.allThreads = new ArrayList<>();
     }
 
     /**
      * Simulates multithreaded environment where multiple customers and vendors interact with the ticket pool
      */
-    public void simulate(){
-        for (int i=1; i<=config.getNumOfVendors(); i++){
-            String name = "Vendor_#" + i;
-            Runnable vendor = new Vendor(this.ticketPool);
-            Thread vendorThread = new Thread(vendor, name );
-            vendorThread.start();
+    public void simulate() {
+        // create runnables to simulate multiple vendors and customers
+        Runnable runVendor = new VendorRunner(this.ticketPool);
+        Runnable runCustomer = new CustomerRunner(this.ticketPool);
+        // create threads
+        Thread runCustomerThread = new Thread(runCustomer, "Run customer thread");
+        Thread runVendorThread = new Thread(runVendor, "Run customer thread");
+        // start threads
+        runCustomerThread.start();
+        runVendorThread.start();
 
-            vendorThreads.add(vendorThread);
-            allThreads.add(vendorThread);
+        try {
+        // make sure that all customer & vendor threads are executed before moving to the next lines of code
+            runCustomerThread.join();
+            runVendorThread.join();
+        } catch (InterruptedException e) {
+            logInfo(logger,"Error when joining.", "SEVERE");
         }
-
-        for (int i=1; i<=config.getNumOfCustomers(); i++){
-            String name = "Customer_#" + i;
-            Runnable customer = new Customer(this.ticketPool);
-            Thread customerThread = new Thread(customer, name );
-            customerThread.start();
-
-            customerThreads.add(customerThread);
-            allThreads.add(customerThread);
-        }
-
-        // makes all the threads to complete first before continuing to execute the rest of the code in the main method;
-        for (Thread thread : allThreads){
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                logInfo(logger, "issue while joining", "SEVERE");
-            }
-        }
-
         logInfo(logger,"Simulation execution completed successfully", "INFO");
         System.out.println(LoggerHandler.getLogs());
+
+    }
+
+    public static void startThread(TicketPool ticketPool, String threadType, int timeout, int threadNum) {
+        Runnable runnable = null;
+        //create a runnable instance
+        if (threadType.equalsIgnoreCase("Customer")) {
+            runnable = new Customer(ticketPool);
+        } else if (threadType.equalsIgnoreCase("Vendor")) {
+            runnable = new Vendor(ticketPool);
+        }
+        Thread thread = new Thread(runnable, threadType + "_#" + threadNum); // create the thread
+        thread.start();
+        try {
+            Thread.sleep(timeout); // sleep for the specified timeout
+        } catch (InterruptedException e) {
+            logInfo(logger,"Error occured while sleeping for specified timeout", "SEVERE");
+        }
 
     }
 
@@ -72,3 +75,24 @@ public class Simulator {
         return ticketPool;
     }
 }
+
+//          VERY IMPORTANT USEFUL CODE
+//        // Runnable interface implementation to insantiate multiple vendor threads
+//        Runnable runVendor = new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i=1; i<=config.getNumOfVendors(); i++){
+//                    startThread("Vendor", config.getTicketReleaseRate(), i);
+//                }
+//            }
+//        };
+//
+//        // Runnable interface implementation to insantiate multiple customer threads
+//        Runnable runCustomer = new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i=1; i<=config.getNumOfCustomers(); i++){
+//                    startThread("Customer", config.getCustomerRetrievalRate(), i);
+//                }
+//            }
+//        };
