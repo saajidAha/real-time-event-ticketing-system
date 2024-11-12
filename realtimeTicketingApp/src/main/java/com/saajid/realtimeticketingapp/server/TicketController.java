8257dce44c52a2d2c330b9ae50c89407b82ad71f
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-13 02:12:12 +0530

    added basic routes to frontend and added CORS support

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TicketController.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TicketController.java
@@ -1,14 +1,41 @@
 package com.saajid.realtimeticketingapp.server;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.saajid.realtimeticketingapp.mainLogic.Configuration;
+import com.saajid.realtimeticketingapp.mainLogic.LoggerHandler;
+import com.saajid.realtimeticketingapp.mainLogic.Simulator;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import static com.saajid.realtimeticketingapp.mainLogic.LoggerHandler.logInfo;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Allow requests from React dev server
 @RequestMapping("/")
 public class TicketController {
-    @GetMapping("")
-    public String getName(){
-        return "<h1>Hello This springboot project is working!</h1>";
+    private static Logger logger = Logger.getLogger(TicketController.class.getName());
+
+    static{
+        logger.addHandler(LoggerHandler.getFileHandler());
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from springboot backend!";
+    }
+
+    @GetMapping("/simulate")
+    public ArrayList<String> getName(){
+        return LoggerHandler.getLogs();
+    }
+
+    @PostMapping("/simulate")
+    public void simulate(@RequestBody Configuration config){
+        logInfo(logger, "Simulation request recieved from the front-end successfully.","INFO");
+        LoggerHandler.clearLogs();
+        config.serialize();
+        Simulator simulator = new Simulator(config);
+        simulator.simulate();
     }
 }
