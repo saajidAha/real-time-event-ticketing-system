0909c982e65c4f2b39e4d8cd588fca77c9b4bfd4
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-30 20:33:42 +0530

    added additional endpoints

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TicketController.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TicketController.java
@@ -2,10 +2,11 @@ package com.saajid.realtimeticketingapp.server;
 
 import com.saajid.realtimeticketingapp.mainLogic.Configuration;
 import com.saajid.realtimeticketingapp.mainLogic.LoggerHandler;
-import com.saajid.realtimeticketingapp.mainLogic.Simulator;
+import com.saajid.realtimeticketingapp.mainLogic.Ticket;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 import static com.saajid.realtimeticketingapp.mainLogic.LoggerHandler.logInfo;
@@ -14,28 +15,45 @@ import static com.saajid.realtimeticketingapp.mainLogic.LoggerHandler.logInfo;
 @CrossOrigin(origins = "http://localhost:3000") // Allow requests from React dev server
 @RequestMapping("/")
 public class TicketController {
+    private TicketService ticketService;
     private static Logger logger = Logger.getLogger(TicketController.class.getName());
+
+    public TicketController(TicketService ticketService){
+        this.ticketService = ticketService;
+    }
 
     static{
         logger.addHandler(LoggerHandler.getFileHandler());
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello from springboot backend!";
+    @GetMapping("/logs")
+    public ArrayList<String> getLogs(){
+        return this.ticketService.getLogs();
     }
 
-    @GetMapping("/simulate")
-    public ArrayList<String> getName(){
-        return LoggerHandler.getLogs();
+    @PostMapping("/tickets/add")
+    public void addTicket(@RequestBody Ticket ticket){
+        this.ticketService.addTicket(ticket);
+    }
+
+    @PostMapping("/tickets/remove")
+    public void removeTicket(){
+        this.ticketService.removeTicket();
+    }
+
+    @PostMapping("/tickets/createpool")
+    public void createTicketPool(@RequestBody Configuration config){
+        this.ticketService.createTicketPool(config);
+    }
+
+    @GetMapping("/tickets")
+    public Vector<Ticket> getTicketsList(){
+        return this.ticketService.getTicketsList();
     }
 
     @PostMapping("/simulate")
     public void simulate(@RequestBody Configuration config){
         logInfo(logger, "Simulation request recieved from the front-end successfully.","INFO");
-        LoggerHandler.clearLogs();
-        config.serialize();
-        Simulator simulator = new Simulator(config);
-        simulator.simulate();
+        this.ticketService.simulate(config);
     }
 }
