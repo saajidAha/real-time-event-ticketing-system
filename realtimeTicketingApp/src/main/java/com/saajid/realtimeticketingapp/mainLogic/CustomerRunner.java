132b3f1939d7b16f3926f98157bf8c1f97fe80e7
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-30 20:36:39 +0530

    removed execution logic from simulator and added it to the run method and made some refinements and done tests

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/CustomerRunner.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/CustomerRunner.java
@@ -1,5 +1,7 @@
 package com.saajid.realtimeticketingapp.mainLogic;
 
+import static com.saajid.realtimeticketingapp.mainLogic.LoggerHandler.logInfo;
+
 /**
  * This class is responsible for the creation of multiple customer threads
  */
@@ -16,7 +18,17 @@ public class CustomerRunner implements Runnable {
     @Override
     public void run(){
         for (int i=1; i<=this.ticketPool.getConfig().getNumOfCustomers(); i++){
-            Simulator.startThread( this.ticketPool,"Customer", this.ticketPool.getConfig().getCustomerRetrievalRate(), i );
+            if (!TicketPool.customerThreadCanRun){
+                break;
+            }
+            Thread thread = new Thread( new Customer(ticketPool) , "Customer_#" + i); // create the thread
+            thread.start();
+            try {
+                Thread.sleep(this.ticketPool.getConfig().getCustomerRetrievalRate()); // sleep for the specified timeout
+                thread.join(); // wait till thread completes its task to proceed
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/VendorRunner.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/VendorRunner.java
@@ -4,7 +4,8 @@ package com.saajid.realtimeticketingapp.mainLogic;
  * This class is responsible for the creation of multiple Vendor threads
  */
 public class VendorRunner implements Runnable {
-    TicketPool ticketPool;
+    private TicketPool ticketPool;
+
     public VendorRunner(TicketPool ticketPool){
         this.ticketPool = ticketPool;
     }
@@ -15,7 +16,17 @@ public class VendorRunner implements Runnable {
     @Override
     public void run(){
         for (int i=1; i<=ticketPool.getConfig().getNumOfVendors(); i++){
-            Simulator.startThread( this.ticketPool, "Vendor", this.ticketPool.getConfig().getTicketReleaseRate(), i );
+            if (!TicketPool.vendorThreadCanRun){
+                break;
+            }
+            Thread thread = new Thread( new Vendor(ticketPool) , "Vendor_#" + i); // create the thread
+            thread.start();
+            try {
+                Thread.sleep(this.ticketPool.getConfig().getTicketReleaseRate()); // sleep for the specified timeout
+                thread.join(); // wait till thread completes its task to proceed
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
