040cff38c921f6e441c3d7fec2363fb07674e0ba
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-30 20:38:41 +0530

    added logic to start cli and server independently based on user input

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/RealtimeTicketingAppApplication.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/RealtimeTicketingAppApplication.java
@@ -1,5 +1,6 @@
 package com.saajid.realtimeticketingapp;
 
+import com.saajid.realtimeticketingapp.commandLine.CommandLineInterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealtimeTicketingAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RealtimeTicketingAppApplication.class, args);
+//		Run the CLI until user wants to exit
+		do{
+			CommandLineInterface.init();
+		}
+		while(CommandLineInterface.checkYesNoResponse("Do you want to use the command line again?"));
+
+//		Start the springboot server based on user response else terminate the program
+		if (CommandLineInterface.checkYesNoResponse("Do you want to start the SpringBoot Server?")){
+			SpringApplication.run(RealtimeTicketingAppApplication.class, args);
+		}
+		else{
+			System.out.println("Program terminated successfully.");
+		}
 	}
 
 }
