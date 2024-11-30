5925c37aef66bfe295b1e013f0c19e3cb3a062bf
Saajid Ahamed <saajidahamed1@gmail.com>
2024-11-30 16:42:12 +0530

    added TicketService class with tested methods

---
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TicketService.java
@@ -0,0 +1,66 @@
+package com.saajid.realtimeticketingapp.server;
+
+import com.saajid.realtimeticketingapp.mainLogic.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+@Service
+public class TicketService {
+    /**
+     * Static TicketPool object for the purpose of being used as reference by specific methods
+     */
+    private static TicketPool ticketPool;
+
+    /**
+     * Gets the logs and returns it
+     * @return Logs related to the system activity
+     */
+    public ArrayList<String> getLogs(){
+        return LoggerHandler.getLogs();
+    }
+
+    /**
+     * Runs simulation based on the configuration
+     * @param config Configuration object
+     */
+    public void simulate(Configuration config){
+        LoggerHandler.clearLogs();
+        config.serialize();
+        Simulator simulator = new Simulator(config);
+        simulator.simulate();
+        ticketPool = simulator.getTicketPool();
+    }
+
+    /**
+     * Creates a TicketPool object
+     * @param config Configuration object
+     */
+    public void createTicketPool(Configuration config){
+        ticketPool = new TicketPool(config);
+    }
+
+    /**
+     * Adds a ticket to the ticketpool array
+     * @param ticket Ticket object
+     */
+    public void addTicket(Ticket ticket){
+        ticketPool.addTicket(ticket);
+    }
+
+    /**
+     * Removes the first ticket from the ticketpool array
+     */
+    public void removeTicket(){
+        ticketPool.removeTicket();
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Vector<Ticket> getTicketsList(){
+        return ticketPool.getTickets();
+    }
+}
