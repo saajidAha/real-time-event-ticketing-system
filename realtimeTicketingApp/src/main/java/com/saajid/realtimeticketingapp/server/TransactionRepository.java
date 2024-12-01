c0516ffb11175b935ab4c66232d5922d723dd54a
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-01 20:54:31 +0530

    added repo for database interaction

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TransactionRepository.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.saajid.realtimeticketingapp.server;
 
-import com.saajid.realtimeticketingapp.mainLogic.Ticket;
+import com.saajid.realtimeticketingapp.mainLogic.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
  * Class responsible for handling CRUD operations with the database
  */
 @Repository
-public interface TransactionRepository extends JpaRepository<Ticket, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
 //    all the neccessary database methods are already implemented by spring.
 }
