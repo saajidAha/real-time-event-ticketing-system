da93472d27643fea1c3ce2531e71e4be35a63faa
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-01 12:57:50 +0530

    added repo class for database operations

---
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/server/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.saajid.realtimeticketingapp.server;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Class responsible for handling CRUD operations with the database
+ */
+@Repository
+public class TicketRepository {
+}
