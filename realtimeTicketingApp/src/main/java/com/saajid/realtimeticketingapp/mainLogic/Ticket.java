4520dea08358c80b1ef22e003a7a863d3075f7e5
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-01 20:51:01 +0530

    added lombok and reduced boilerplate code

--- realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Ticket.java
+++ realtimeTicketingApp/src/main/java/com/saajid/realtimeticketingapp/mainLogic/Ticket.java
@@ -1,6 +1,7 @@
 package com.saajid.realtimeticketingapp.mainLogic;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID; // required for random id generation
 
@@ -8,14 +9,8 @@ import java.util.UUID; // required for random id generation
  * This class represents the ticket entity that will be released by vendors and purchased by customers
  */
 @Data // auto generate getters & setters
+@NoArgsConstructor
 public class Ticket {
-    private String ticketID;
-
-    /**
-     * Constructor to initialize ticket
-     */
-    public Ticket () {
-        // Generate a random ID with 8 characters
-        this.ticketID = UUID.randomUUID().toString().substring(0,8);
-    }
+    // Generate a random ID with 8 characters
+    private String ticketID = UUID.randomUUID().toString().substring(0,8);
 }
