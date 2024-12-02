ecb2a9016b7120164c1651480411f9db9d2dfed9
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-02 17:39:15 +0530

    additional features to log component

--- ticketingFrontEnd/src/components/LogInfo.jsx
+++ ticketingFrontEnd/src/components/LogInfo.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const Home = () => {
+const LogInfo = () => {
     const [data, setData] = useState([]);
-
     const fetchData = async () => {
         try {
-            const response = await axios.get('http://localhost:8080/simulate');
-            console.log(response.data);
+            const response = await axios.get('http://localhost:8080/logs');
+            // console.log(response.data);
             setData(response.data);
         } catch (error) {
             console.log(error.message);
@@ -15,15 +14,16 @@ const Home = () => {
     };
 
     useEffect(() => {
-        setInterval(fetchData, 1000 )// Call fetchData when the component mounts
+        let intervalID = setInterval(fetchData, 1500 ) // Call fetchData when the component mounts
+        return () => {clearInterval(intervalID)};
     }, []);
 
     return (
-        <>
+        <div style={{border:"solid", width:"500px"}}>
             This is the data: { data.map( (item, index) => <h4 key={index}>{item}</h4>) }
             <button onClick={fetchData}>Get Data</button>
-        </>
+        </div>
     );
 };
 
-export default Home;
+export default LogInfo;
