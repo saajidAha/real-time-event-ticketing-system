7aa7290fa715e4b5709b9b361e2c15a15eba42ac
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-08 22:46:45 +0530

    added navbar

--- ticketingFrontEnd/src/layouts/MainLayout.jsx
+++ ticketingFrontEnd/src/layouts/MainLayout.jsx
@@ -3,11 +3,13 @@ import Footer from "../components/Footer.jsx";
 import {Outlet} from "react-router-dom";
 import TicketDisplay from "../components/TicketDisplay.jsx";
 import LogDisplay from "../components/LogDisplay.jsx";
+import NavBar from "../components/NavBar.jsx";
 
 const MainLayout = () => {
     return(
         <>
             <Header />
+            <NavBar />
             <Outlet />
             <TicketDisplay />
             <LogDisplay />
