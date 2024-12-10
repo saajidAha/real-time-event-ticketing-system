56123dec1126158d8704bc076b97765e2723c8bd
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-10 19:02:47 +0530

    added color to highlight navbar route name based on the current route

--- ticketingFrontEnd/src/components/NavBar.jsx
+++ ticketingFrontEnd/src/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import {NavLink} from "react-router-dom";
 // NavBar to display all of the route links for smooth page navigation
 const NavBar = () => {
+    const applyNavClickStyle = ({ isActive }) => ({
+        color: isActive ? "yellow" : "white",
+    })
     return(
         <div className="flex flex-row justify-evenly py-2 bg-gray-700 ">
-            <NavLink to='/' className=" rounded  text-white font-semibold">Simulation Page</NavLink>
-            <NavLink to='/customer' className=" rounded  text-white font-semibold">Customer Page</NavLink>
-            <NavLink to='/vendor' className=" rounded  text-white font-semibold">Vendor Page</NavLink>
-            <NavLink to='/ticketpool' className=" ounded  text-white font-semibold">TicketPool Initialization</NavLink>
+            <NavLink to='/' className=" rounded  text-white font-semibold" style={applyNavClickStyle}>Simulation Page</NavLink>
+            <NavLink to='/customer' className=" rounded  text-white font-semibold" style={applyNavClickStyle}>Customer Page</NavLink>
+            <NavLink to='/vendor' className=" rounded  text-white font-semibold" style={applyNavClickStyle}>Vendor Page</NavLink>
+            <NavLink to='/ticketpool' className=" ounded  text-white font-semibold" style={applyNavClickStyle}>TicketPool Initialization</NavLink>
         </div>
     )
 }
